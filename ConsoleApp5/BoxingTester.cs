5425f94aa6bf6d68dfce5ded50c176029dc9ce0c
sofis <sofiia.shevchenko.25@kse.org.ua>
2026-03-28 16:33:39 +0200

    second stage

---
+++ ConsoleApp5/BoxingTester.cs
@@ -0,0 +1,15 @@
+﻿namespace Variant2;
+public static class BoxingTester
+{
+    public static void Test()
+    {
+        int a = 10; //створюємо int
+        object A = a; //копіюємо a в object(купа)
+        int unA = (int)A; //дістаєм значення a назад 
+        
+        Console.WriteLine($"a = {a}");
+        Console.WriteLine($"boxedA = {A}");
+        Console.WriteLine($"unboxedA = {unA}");
+        
+    }
+}
