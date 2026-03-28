526294b456b773bef545d8d8387d0cb8119fe0a4
sofis <sofiia.shevchenko.25@kse.org.ua>
2026-03-28 16:09:03 +0200

    second stage

---
+++ ConsoleApp5/ResourceManage.cs
@@ -0,0 +1,29 @@
+﻿namespace Variant1;
+
+using System.Collections.Generic;
+
+public class ResourceManager<T> where T : Resource
+{
+    private readonly List<T> _resources = new();
+    
+    public void Add(T resource)
+    {
+        _resources.Add(resource);
+    }
+    
+    public void OpenAll()
+    {
+        foreach (T t in _resources)
+        {
+            t.Open();
+        }
+    }
+    
+    public void CloseAll()
+    {
+        foreach (T t in _resources)
+        {
+            t.Close();
+        }
+    }
+}
