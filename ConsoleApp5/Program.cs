647155d9318916c813a7c1332b2aec589fc62cda
sofis <sofiia.shevchenko.25@kse.org.ua>
2026-03-28 17:01:11 +0200

    third stage

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ConsoleApp5;
 
-Console.WriteLine("Hello, World!");
+var container = new DataContainer<DataItem>();
+for (int i = 0; i < 10000; i++)
+{
+    var item = new DataItem
+    {
+        Id = $"item-{i}",
+        Payload = new byte[1024]
+    };
+    container.AddItem(item);
+}
+Console.WriteLine($"Total size before GC: {container.GetTotalSize()} bytes");
+GC.Collect();
+Console.WriteLine("GC.Collect() was called.");
+Console.WriteLine($"Total size after GC: {container.GetTotalSize()} bytes");
