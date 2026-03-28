1a4b6b98babb83e6ef4bdc86f74612252c50134d
sofis <sofiia.shevchenko.25@kse.org.ua>
2026-03-28 16:57:14 +0200

    second stage

---
+++ ConsoleApp5/DataContainer.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp5;
+
+using System.Collections.Generic;
+
+public class DataContainer<T> where T : DataItem
+{
+    private readonly List<T> _items = new();
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+    public long GetTotalSize()
+    {
+        long all = new();
+        foreach (T items in _items)
+        {
+            all += items.GetSize();
+        }
+
+        return all;
+    }
+
+    
+}
