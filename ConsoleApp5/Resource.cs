ad2d303a6c836176689741633f06196c9a8ee535
sofis <sofiia.shevchenko.25@kse.org.ua>
2026-03-28 16:08:11 +0200

    first stage

---
+++ ConsoleApp5/Resource.cs
@@ -0,0 +1,15 @@
+﻿namespace Variant1;
+public abstract class Resource
+{
+    public string Name { get; }
+    public bool IsOpen { get; protected set; }
+    public abstract void Open();
+    public abstract void Close();
+    
+    protected Resource(string name)
+    {
+        Name = name;
+        IsOpen = false;
+    }
+    
+}
